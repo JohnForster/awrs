9585a69c1cc74ea28bdf27a6eb45f8d1398edc32
Andrew Page <ampage24@gmail.com>
2021-11-20 17:48:01 +0000

    Started splitting code into multiple files. Noticed bug when moving cursor off left

---
+++ src/awrs/sprite_loading.rs
@@ -0,0 +1,96 @@
+use bevy::prelude::*;
+
+#[derive(Default)]
+pub struct TerrainAtlas {
+    texture_handle: Handle<Texture>,
+    pub atlas_handle: Handle<TextureAtlas>,
+}
+
+#[derive(Default)]
+pub struct UIAtlas {
+    texture_handle: Handle<Texture>,
+    pub atlas_handle: Handle<TextureAtlas>,
+}
+
+#[derive(Default)]
+pub struct UnitAtlas {
+    texture_handle: Handle<Texture>,
+    pub atlas_handle: Handle<TextureAtlas>,
+}
+
+pub fn load_ui_sprites(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    info!("Loading UI Sprites");
+    let texture_handle = asset_server.load("UISprites.png");
+    let mut texture_atlas = TextureAtlas::new_empty(texture_handle.clone(), Vec2::new(143.0, 64.0));
+
+    let cursor_rect = bevy::sprite::Rect {
+        min: Vec2::new(44.0, 6.0),
+        max: Vec2::new(44.0 + 28.0, 6.0 + 32.0),
+    };
+
+    texture_atlas.add_texture(cursor_rect);
+    let atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.insert_resource(UIAtlas {
+        texture_handle,
+        atlas_handle,
+    })
+}
+
+pub fn load_unit_sprites(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    info!("Loading Unit Sprites");
+    let texture_handle = asset_server.load("unitSprites.png");
+    let mut texture_atlas = TextureAtlas::new_empty(texture_handle.clone(), Vec2::new(349.0, 93.0));
+
+    let inf_orange_sprite = bevy::sprite::Rect {
+        min: Vec2::new(24.0, 4.0),
+        max: Vec2::new(24.0 + 14.0, 4.0 + 16.0),
+    };
+
+    texture_atlas.add_texture(inf_orange_sprite);
+    let atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.insert_resource(UnitAtlas {
+        texture_handle,
+        atlas_handle,
+    })
+}
+
+pub fn load_terrain_sprites(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    info!("Loading Terrain Sprites");
+    // Terrain Sprites
+    let texture_handle = asset_server.load("sprites.png");
+    let mut texture_atlas =
+        TextureAtlas::new_empty(texture_handle.clone(), Vec2::new(1215.0, 1744.0));
+
+    let grass_rect = bevy::sprite::Rect {
+        min: Vec2::new(217.0, 1567.0),
+        max: Vec2::new(217.0 + 16.0, 1567.0 + 16.0),
+    };
+    let sea_rect = bevy::sprite::Rect {
+        min: Vec2::new(340.0, 1567.0),
+        max: Vec2::new(340.0 + 16.0, 1567.0 + 16.0),
+    };
+
+    texture_atlas.add_texture(sea_rect);
+    texture_atlas.add_texture(grass_rect);
+
+    let atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.insert_resource(TerrainAtlas {
+        texture_handle,
+        atlas_handle,
+    })
+}
