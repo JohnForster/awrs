9405609eef50bee7ea21e7c097bdf0c4f898d0b2
John Forster <johnforster@makers.tech>
2021-11-27 17:21:20 +0000

    Load unit data from .ron file

--- src/awrs/game.rs
+++ src/awrs/game.rs
@@ -1,5 +1,5 @@
-
 use bevy::prelude::*;
+use bevy_asset_ron::RonAssetPlugin;
 
 use super::cursor::*;
 use super::map::*;
@@ -20,15 +20,17 @@ enum GameState {
 }
 
 use super::sprite_loading::*;
+use super::unit::*;
 
 pub struct AWRSPlugin;
 
 impl Plugin for AWRSPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app
+        app.add_plugin(RonAssetPlugin::<UnitType>::new(&["ron"]))
             .add_startup_system(load_terrain_sprites.system().label("load_sprites"))
             .add_startup_system(load_unit_sprites.system().label("load_sprites"))
             .add_startup_system(load_ui_sprites.system().label("load_sprites"))
+            .add_startup_system(load_units.system())
             .add_state(GameState::Running) // see if this can go after the next two method calls
             .add_system_set(
                 SystemSet::on_enter(GameState::Running)
@@ -38,6 +40,7 @@ impl Plugin for AWRSPlugin {
             .add_system_set(
                 SystemSet::on_update(GameState::Running)
                     .with_system(handle_cursor_move.system())
-                    .with_system(handle_cursor_select.system()));
+                    .with_system(handle_cursor_select.system()),
+            );
     }
 }
--- src/awrs/unit.rs
+++ src/awrs/unit.rs
@@ -1,17 +1,80 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, reflect::TypeUuid};
+use serde::{Deserialize, Serialize};
 
 use super::cell::*;
 
-#[derive(Clone)]
+#[derive(Serialize, Deserialize, Debug, TypeUuid)]
+#[uuid = "5386B529-81CC-405A-9600-CB51B83F8CC9"]
+pub enum UnitTag {
+    Biological,
+    Mechanical,
+    Light,
+    Ground,
+    Air,
+    Infantry,
+}
+
+#[derive(Serialize, Deserialize, Debug, TypeUuid)]
+#[uuid = "534753A6-C796-4792-885E-A52C7D7CBF07"]
+pub struct Bonus {
+    pub tag: UnitTag,
+    pub additional_damage: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug, TypeUuid)]
+#[uuid = "76D0BCF9-21FD-451F-8C1F-D0E600A58D0A"]
+pub enum Directness {
+    Melee,
+    Ranged(f32, f32), // Min, Max
+}
+
+#[derive(Serialize, Deserialize, Debug, TypeUuid)]
+#[uuid = "21CD8CEB-ED1B-4D20-921D-B775C4E31DBF"]
+pub struct Weapon {
+    pub id: usize,
+    pub name: String,
+    pub directness: Directness, // TODO Come up with better name
+    pub base_damage: f32,
+    pub num_of_attacks: f32,
+    pub bonuses: Vec<Bonus>,
+    pub applicable: Vec<UnitTag>,
+}
+
+#[derive(Serialize, Deserialize, Debug, TypeUuid)]
+#[uuid = "67B15859-CC4B-4C35-AB9C-5856628833E4"]
+pub struct UnitType {
+    pub id: usize,
+    pub name: String,
+    pub max_health: f32,
+    pub max_ammo: f32,
+    pub max_fuel: f32,
+    pub tags: Vec<UnitTag>,
+    pub base_armour: f32,
+    // pub weapon_one: Option<Weapon>,
+    // pub weapon_two: Option<Weapon>,
+}
+
 pub struct Unit {
-    pub unit_type: UnitType,
+    pub unit_type: usize, // ? Do we need
     pub team: Team,
     pub location: Cell,
-    pub health: UnitHealth, // etc. etc..
+    pub health: UnitHealth,
+    // pub ammo: Ammo,
+    // etc. etc..
+}
+
+pub struct UnitHandle {
+    pub handle: Handle<UnitType>,
+}
+
+pub fn load_units(server: Res<AssetServer>, mut commands: Commands) {
+    // load our item configs!
+    let handle = server.load("units/infantry.ron");
+    commands.insert_resource(UnitHandle { handle });
 }
 
 #[derive(Clone)]
-pub struct UnitHealth(pub u32);
+pub struct UnitHealth(pub f32);
 
 // Or, to avoid pub
 // impl From<u32> for UnitHealth {
@@ -25,11 +88,6 @@ pub struct UnitHealth(pub u32);
 //         health.0
 //     }
 // }
-
-#[derive(Clone)]
-pub enum UnitType {
-    Infantry,
-}
 
 #[derive(Clone)]
 pub struct Team(pub u32);
