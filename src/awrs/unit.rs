7c8bd554587c7a68009c9a3b87fafabcddf02d51
John Forster <johnforster@makers.tech>
2021-11-29 15:00:26 +0000

    Allow unit movement

--- src/awrs/unit.rs
+++ src/awrs/unit.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use super::cell::Cell;
+use super::{cell::Cell, constants::TILE_SIZE};
 
 #[derive(Clone)]
 pub struct UnitHealth(pub f32);
@@ -34,6 +34,8 @@ pub struct Team(pub u32);
 //     }
 // }
 
+pub struct Selected;
+
 pub struct Unit {
     pub unit_type: usize,
     pub team: Team,
@@ -49,4 +51,36 @@ pub struct UnitBundle {
     pub data: Unit,
     #[bundle]
     pub sprite: SpriteSheetBundle,
+}
+
+// Very similar to moving cursor.
+// Could have Movable struct component so that this can be reused?
+// Or could extract movement logic into a separate function?
+pub fn handle_unit_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut unit_query: Query<(&mut Transform, &mut Unit), With<Selected>>,
+) {
+    info!("Unit movement system goes brrrr");
+    for (mut transform, mut unit) in unit_query.iter_mut() {
+        info!("Units go brrrrr");
+        if keyboard_input.just_pressed(KeyCode::W) {
+            transform.translation.y += 1.0 * TILE_SIZE;
+            unit.location.y += 1;
+        }
+
+        if keyboard_input.just_pressed(KeyCode::A) {
+            transform.translation.x -= 1.0 * TILE_SIZE;
+            unit.location.x -= 1;
+        }
+
+        if keyboard_input.just_pressed(KeyCode::S) {
+            transform.translation.y -= 1.0 * TILE_SIZE;
+            unit.location.y -= 1;
+        }
+
+        if keyboard_input.just_pressed(KeyCode::D) {
+            transform.translation.x += 1.0 * TILE_SIZE;
+            unit.location.x += 1;
+        }
+    }
 }
